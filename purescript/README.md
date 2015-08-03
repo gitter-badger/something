9a702010876421eeb64c55146223d2c60788b4d3
aspidites <emarshall85@gmail.com>
2015-08-03 17:00:24 -0400

    Added explicit imports for documentation sake

--- purescript/README.md
+++ purescript/README.md
@@ -2,4 +2,4 @@
 - Library I used (purescript-node-readline) was missing a function, so I added
   it using FFI in ./src/Main.js
 - `index.js` may be run from the command line with `node index.js` and was
-  created with `pulp browserify -O --to index.js`.
+  created with `pulp build -O --to index.js`.
--- purescript/src/Main.purs
+++ purescript/src/Main.purs
@@ -1,11 +1,12 @@
 module Main where
 
-import Prelude
-import Node.ReadLine
-import Control.Monad.Eff
-import Control.Monad.Eff.Console
-import Control.Monad.Eff.Random
-import Unsafe.Coerce
+import Prelude (($), (++), flip, bind, compare, Ordering(..))
+import Node.ReadLine (Interface(..), 
+                      createInterface, noCompletion, prompt, setLineHandler)
+import Control.Monad.Eff (Eff(..))
+import Control.Monad.Eff.Console (CONSOLE(..), log)
+import Control.Monad.Eff.Random (randomInt)
+import Unsafe.Coerce (unsafeCoerce)
 
 foreign import closeInterface 
   :: forall eff. Interface -> Eff (console :: CONSOLE | eff) Interface
@@ -13,10 +14,12 @@ foreign import closeInterface
 main = do
   interface <- createInterface noCompletion
   secret_number <- randomInt 1 100
+  log "Guess the number!"
 
+  log "Please input your guess."
   prompt interface
   flip setLineHandler interface $ \guess -> do
-      log $ "You typed: " ++ guess
+      log $ "You guessed: " ++ guess
       case unsafeCoerce guess `compare` secret_number of
         LT -> do
           log "Too small!"
